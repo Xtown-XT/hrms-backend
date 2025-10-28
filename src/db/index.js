9342f2c2ab6fca1543bb136614c49efa76776537
jothi-xtown <jothi@xtowns.in>
2025-10-28 10:02:26 +0530

    employee

--- src/db/index.js
+++ src/db/index.js
@@ -32,17 +32,17 @@ const Sequelize = SequelizePkg; // <-- fix here
 const { DataTypes, Op } = SequelizePkg;
 
 // Create Sequelize instance
-// const sequelize = new Sequelize("hrms_demo", "ramya", "ramya", {
-//   host: "192.168.1.150",
-//   port: 3306,
-//   dialect: "mysql",
-// });
-
-const sequelize = new Sequelize("hrmsback", "root", "Sanjay@1218", {
-  host: "127.0.0.1",
+const sequelize = new Sequelize("hrms", "ramya", "ramya", {
+  host: "192.168.1.150",
   port: 3306,
   dialect: "mysql",
 });
+
+// const sequelize = new Sequelize("hrmsback", "root", "Sanjay@1218", {
+//   host: "127.0.0.1",
+//   port: 3306,
+//   dialect: "mysql",
+// });
 
 // Authenticate database connection inside async IIFE
 (async () => {
--- src/db/xtown.js
+++ src/db/xtown.js
@@ -6,17 +6,17 @@ const { DataTypes, Op } = SequelizePkg;
 
 
 // Create Sequelize instance
-// const sequelize =new Sequelize("att", "ramya", "ramya", {
-//   host: "192.168.1.150",
-//   port: 3306,
-//     dialect: "mysql",
-// });
-
-const sequelize =new Sequelize("xtown", "root", "Sanjay@1218", {
-  host: "127.0.0.1",
+const sequelize =new Sequelize("att", "ramya", "ramya", {
+  host: "192.168.1.150",
   port: 3306,
     dialect: "mysql",
 });
+
+// const sequelize =new Sequelize("xtown", "root", "Sanjay@1218", {
+//   host: "127.0.0.1",
+//   port: 3306,
+//     dialect: "mysql",
+// });
 
 // Authenticate database connection inside async IIFE
 (async () => {
